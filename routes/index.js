49aeacf17df15714166a1c9a7c3eb735ea47f0fb
Vitor Girotto <vjpgvjpgvjpgvjpg@gmail.com>
2024-12-24 01:03:11 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../db");
 
+/* GET home page. */
 router.get('/', async (req, res, next) => {
   const result = await db.find();
   res.render('index', { title: 'Express', result });
